bc320d67ae66140350bad027b23aa4c7909c0bc6
cjstronger <clintjstrong@gmail.com>
2024-07-08 13:51:54 -0700

    fixed a bug

--- app/artwork/[...id]/page.js
+++ app/artwork/[...id]/page.js
@@ -38,6 +38,7 @@ export default function Page({ params }) {
                   fill
                   blur={true}
                   className="object-cover"
+                  key={i}
                 />
               );
             })
