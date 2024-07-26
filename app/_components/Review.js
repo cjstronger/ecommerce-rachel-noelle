98931ca387f48fab665214be36d7c85ed085ee03
cjstronger <clintjstrong@gmail.com>
2024-07-25 18:20:39 -0700

    added actual images to the reviews

--- app/_components/Review.js
+++ app/_components/Review.js
@@ -3,10 +3,16 @@ import Image from "next/image";
 export default function Review({ review }) {
   const { fullName, reviewText, img } = review;
   return (
-    <div className="snap-start flex flex-col justify-center gap-5 p-5 items-center bg-accent min-w-[100%] h-[60vh] text-bg">
+    <div className="snap-start flex flex-col justify-center gap-5 p-2 items-center bg-accent min-w-[100%] h-[60vh] text-bg">
       {img ? (
-        <div className="rounded-full size-11">
-          <Image src={img} fill alt={fullName} />
+        <div className="rounded-full size-[6rem] relative border-2 border-bg">
+          <Image
+            src={img}
+            fill
+            alt={fullName}
+            className="object-cover rounded-full"
+            sizes={"18vw"}
+          />
         </div>
       ) : (
         <div className="rounded-full h-[5rem] w-[5rem] bg-bgDark flex items-center justify-center text-center">
