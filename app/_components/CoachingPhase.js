366fcf34a38746ef28539fdad6dc8c675b28153b
cjstronger <clintjstrong@gmail.com>
2024-08-16 14:17:05 -0700

    changed phases to look better on smallest devices

--- app/_components/CoachingPhase.js
+++ app/_components/CoachingPhase.js
@@ -1,10 +1,10 @@
 export default function CoachingPhase({ title, phase, children }) {
   return (
     <div className="flex items-center gap-5 text-bg">
-      <div className="md:h-[400px] md:w-[400px] w-[280px] h-[300px] bg-accent lg:p-8 p-4 flex flex-col mr-4">
+      <div className="md:h-[400px] md:w-[400px] w-[32vh] h-[35vh] bg-accent lg:p-8 p-4 flex flex-col mr-4">
         <h2 className="text-xl md:text-2xl text-center">{phase}</h2>
-        <h1 className="text-3xl md:text-4xl text-center">{title}</h1>
-        <p className="text-[15px] md:text-lg text-center">{children}</p>
+        <h1 className="text-[3vh] md:text-4xl text-center">{title}</h1>
+        <p className="text-[1.65vh] md:text-lg text-center">{children}</p>
       </div>
     </div>
   );
