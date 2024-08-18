c0dd27bfc21135cf14275dc477897dbc5918fd49
cjstronger <clintjstrong@gmail.com>
2024-08-17 23:29:16 -0700

    added the thing

--- app/success/route.js
+++ app/success/route.js
@@ -7,13 +7,16 @@ const stripe = new Stripe(process.env.STRIPE_KEY);
 
 export async function POST(req) {
   const sig = req.headers.get("stripe-signature");
-  const endpointSecret = "whsec_6I55ZpgeawrMDLUbR5sUQJhk7u7bgohM";
 
   let event;
 
   try {
     const body = await req.text();
-    event = stripe.webhooks.constructEvent(body, sig, endpointSecret);
+    event = stripe.webhooks.constructEvent(
+      body,
+      sig,
+      process.env.STRIPE_WEBHOOK
+    );
   } catch (err) {
     if (err) throw new Error("The webhook was not properly initiated");
     return NextResponse.json({ error: "Webhook Error" }, { status: 400 });
