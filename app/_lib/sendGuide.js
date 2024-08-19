40d91daf4c0acb019a82272c98ceb802f1c1a6df
cjstronger <clintjstrong@gmail.com>
2024-08-19 15:26:04 -0700

    added better email for the guide

--- app/_lib/sendGuide.js
+++ app/_lib/sendGuide.js
@@ -2,7 +2,6 @@
 
 import { Resend } from "resend";
 import { addSubscriber } from "./data-services";
-import { supabase } from "./supabase";
 import NoelleGuide from "@/react-email-starter/emails/noelle-sendGuide";
 
 const resend = new Resend(process.env.RESEND_KEY);
@@ -22,26 +21,10 @@ export default async function sendGuide(formData) {
 }
 
 async function sendGuideEmail(appEmail, firstName) {
-  const { data: fileContent, fileError } = await supabase.storage
-    .from("PDFs")
-    .download("/Uplevel Using My Food Guide.pdf");
-  if (fileError) console.error(fileError);
-
-  const base64FileContent = await fileContent.arrayBuffer().then((buffer) => {
-    return Buffer.from(buffer).toString("base64");
-  });
-
   const { data, error } = await resend.emails.send({
     from: "Rachel Noelle <rachel@rachelnoelle.net>",
     to: [`${appEmail}`],
     subject: "My Free Ebook - Rachel Noelle",
-    attachments: [
-      {
-        filename: "Uplevel Using My Food Guide.pdf",
-        content: base64FileContent,
-        type: "application/pdf",
-      },
-    ],
     react: <NoelleGuide firstName={firstName} />,
   });
   if (error)
--- react-email-starter/emails/noelle-sendGuide.jsx
+++ react-email-starter/emails/noelle-sendGuide.jsx
@@ -11,34 +11,43 @@ import {
   Heading,
   Tailwind,
   Link,
+  Button,
 } from "@react-email/components";
 import React from "react";
 
 export const NoelleGuide = ({ firstName }) => {
   return (
     <Html>
-      <Head />
       <Tailwind>
+        <Head />
         <Preview>Rachel's Free Ebook</Preview>
-        <Body className="bg-neutral-900 text-neutral-100 overflow-x-hidden">
+        <Body className="text-[#e7e1d7] bg-[#28282b]">
           <Container>
-            <Section className="absolute bg-neutral-900 h-[12rem] w-[15rem] md:size-full mt-[5rem] p-5">
-              <Heading className="text-4xl">Hello, {firstName}</Heading>
-              <Text className="text-2xl text-center">
-                Is is time to get serious about your health?
+            <Section className="p-5">
+              <Heading className="text-2xl">Hello, {firstName}</Heading>
+              <Text className="text-lg">
+                Here is the free ebook that explains the daily steps I take to
+                prioritize myself and build a better life.
               </Text>
-              <Text className="text-2xl text-center">
-                This is where the journey begins in improving your life.
-              </Text>
+              <Text className="text-lg">Much love, Rachel</Text>
+              <Text className="text-lg">Namaste</Text>
             </Section>
           </Container>
-          <Img
-            src="https://nzszzpxpduixjugtfdla.supabase.co/storage/v1/object/public/email_images/IMG_2759%20(1).jpg"
-            alt="beach"
-            width="auto"
-            height="800"
-            className="mx-auto"
-          />
+          <Link href="https://nzszzpxpduixjugtfdla.supabase.co/storage/v1/object/public/PDFs/Uplevel%20Using%20My%20Food%20Guide.pdf">
+            <Img
+              src="https://nzszzpxpduixjugtfdla.supabase.co/storage/v1/object/public/email_images/foodguidecover.jpg"
+              alt="foodguidecover"
+              width="100%"
+              height="auto"
+              className="mx-auto bg-[#28282b] w-full max-w-md"
+            />
+          </Link>
+          {/* <Link
+            className="p-3 active:p-4 border-[1px] border-neutral-100 text-neutral-100 mx-auto"
+            href="https://nzszzpxpduixjugtfdla.supabase.co/storage/v1/object/public/PDFs/Uplevel%20Using%20My%20Food%20Guide.pdf"
+          >
+            Download Guide
+          </Link> */}
         </Body>
       </Tailwind>
     </Html>
