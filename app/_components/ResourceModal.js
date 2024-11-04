af27ca6e970d0342870eb06ae48e1d0bae0b1e74
cjstronger <clintjstrong@gmail.com>
2024-11-03 23:49:19 -0800

    added things for the subs

--- app/_components/ResourceModal.js
+++ app/_components/ResourceModal.js
@@ -4,23 +4,61 @@ import { ArrowDownCircleIcon } from "@heroicons/react/24/solid";
 import { useState } from "react";
 import Input from "./Input";
 import { useForm } from "react-hook-form";
-import Button from "./Button";
+import toast from "react-hot-toast";
+import { addSubscriber } from "../_lib/data-services";
 
-export default function ResourceModal() {
+export default function ResourceModal({ data, supabaseUrl }) {
   const [modalOpen, setModalOpen] = useState(false);
+  const [applied, setApplied] = useState(false);
   const { formState, register, handleSubmit } = useForm();
   const { errors } = formState;
 
-  function submitEmail(e) {
-    console.log(e.email);
+  async function submitEmail(e) {
+    const { email } = e;
+    const { error } = await addSubscriber({ subEmail: email });
+    if (error) {
+      toast("There was an error subscribing");
+      return;
+    }
+    setApplied(true);
+    setModalOpen(false);
   }
   return (
     <>
+      {data.map((file, i) => (
+        <div
+          key={i}
+          className="flex gap-5 items-center mt-5 flex-col h-[300px]"
+        >
+          {!applied ? (
+            <button
+              onClick={() => {
+                setModalOpen(true);
+              }}
+            >
+              <ArrowDownCircleIcon className="size-10" />
+            </button>
+          ) : (
+            <a
+              target="_blank"
+              href={`${supabaseUrl}/storage/v1/object/public/PDFs/${file.name}`}
+            >
+              <ArrowDownCircleIcon className="size-10" />
+            </a>
+          )}
+          <h2 className="text-md md:text-lg lg:text-xl text-center">{`${file.name
+            .replaceAll("-", " ")
+            .replace(".pdf", "")}`}</h2>
+        </div>
+      ))}
       {modalOpen && (
         <form
           onSubmit={handleSubmit(submitEmail)}
-          className="bg-fadedBlack opacity-95 rounded-md h-[300px] w-[80vw] max-w-[600px] absolute translate-x-[-50%] translate-y-[-50%] top-[50%] left-[50%] backdrop-blur-xl flex flex-col items-center justify-center"
+          className="bg-fadedBlack opacity-95 rounded-md h-[300px] w-[80vw] max-w-[600px] absolute translate-x-[-50%] translate-y-[-50%] top-[40%] left-[50%] backdrop-blur-xl flex flex-col items-center justify-center"
         >
+          <h2 className="text-bg mb-8 text-center">
+            Please enter your email to get access to my free resources!
+          </h2>
           <Input
             label="Email"
             placeholder="Your Email"
@@ -32,18 +70,14 @@ export default function ResourceModal() {
             type="email"
             autoComplete="email"
           />
-          <button type="submit" className="border-2 border-fadedBlack text-bg">
+          <button
+            type="submit"
+            className="border-2 border-fadedBlack text-bg mt-5"
+          >
             Submit
           </button>
         </form>
       )}
-      <button
-        onClick={() => {
-          setModalOpen(true);
-        }}
-      >
-        <ArrowDownCircleIcon className="size-10" />
-      </button>
     </>
   );
 }
--- app/resources/page.js
+++ app/resources/page.js
@@ -1,6 +1,5 @@
 "use server";
 
-import Link from "next/link";
 import { getFiles } from "../_lib/data-services";
 import { supabaseUrl } from "../_lib/supabase";
 import ResourceModal from "../_components/ResourceModal";
@@ -9,17 +8,10 @@ export default async function page() {
   const { data } = await getFiles();
 
   return (
-    <div className="mt-11">
+    <div className="mt-11 mx-2">
       <h1 className="text-center text-3xl">Resources</h1>
-      <div className="grid grid-cols-4 justify-center place-items-center">
-        {data.map((file, i) => (
-          <div key={i} className="flex gap-5 flex-col items-center mt-5">
-            <ResourceModal />
-            <h2 className="text-xl">{`${file.name
-              .replaceAll("-", " ")
-              .replace(".pdf", "")}`}</h2>
-          </div>
-        ))}
+      <div className="grid grid-cols-2 justify-center place-items-center md:grid-cols-4">
+        <ResourceModal data={data} supabaseUrl={supabaseUrl} />
       </div>
     </div>
   );
